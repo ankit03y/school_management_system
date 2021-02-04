8fa7e626689c2a113e1dd7a49ae94a128f56eafb
Ankit Yadav <ankit03y@yahoo.com>
2021-02-04 23:05:40 +0530

    new GUI added with working database

--- js/database.js
+++ js/database.js
@@ -5,13 +5,57 @@ db = new Datastore({ filename: 'db/teacher.db', autoload: true });
 
 
 // Adds a person
-exports.addPerson = function(firstname, lastname) {
+exports.addPerson = function(firstname, lastname,
+  gender,
+  dateOfBirth,
+  dateOfAdmission,
+  rollNumber,
+  contactNumber,
+  alternateNumber,
+  studentAadharNumber,
+  fatherAadharNumber,
+  fatherName,
+  motherAadharNumber,
+  motherName,
+  bloodGroup,
+  admission,
+  religion,
+  category,
+  email,
+  grade,
+  section,
+  house,
+  admissionId,
+  shortBio,
+  studentPhoto) {
 
   // Create the person object
   var person = {
     "firstname": firstname,
-    "lastname": lastname
-  };
+    "lastname": lastname,
+    "gender": gender,
+    "dateOfBirth": dateOfBirth,
+    "dateOfAdmission": dateOfAdmission,
+    "rollNumber": rollNumber,
+    "contactNumber": contactNumber,
+    "alternateNumber": alternateNumber,
+    "studentAadharNumber": studentAadharNumber,
+    "fatherAadharNumber": fatherAadharNumber,
+    "fatherName": fatherName,
+    "motherAadharNumber": motherAadharNumber,
+    "motherName": motherName,
+    "bloodGroup": bloodGroup,
+    "admission": admission,
+    "religion": religion,
+    "category": category,
+    "email": email,
+    "grade": grade,
+    "section": section,
+    "house": house,
+    "admissionId": admissionId,
+    "shortBio": shortBio,
+    "studentPhoto": studentPhoto,
+    };
 
   // Save the person to the database
   db.insert(person, function(err, newDoc) {
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const database = require('./js/database');
 
-window.onload = function() {
+window.onload = function () {
 
   // Populate the table
   populateTable();
@@ -13,16 +13,80 @@ window.onload = function() {
     var lastname = document.getElementById('lastname');
     var gender = document.getElementById('gender');
     var dateOfBirth = document.getElementById('dateOfBirth');
+    var dateOfAdmission = document.getElementById('dateOfAdmission');
+    var rollNumber = document.getElementById('rollNumber');
+    var contactNumber = document.getElementById('contactNumber');
+    var alternateNumber = document.getElementById('alternateNumber');
+    var studentAadharNumber = document.getElementById('studentAadharNumber');
+    var fatherAadharNumber = document.getElementById('fatherAadharNumber');
+    var fatherName = document.getElementById('fatherName');
+    var motherAadharNumber = document.getElementById('motherAadharNumber');
+    var motherName = document.getElementById('motherName');
+    var bloodGroup = document.getElementById('bloodGroup');
+    var admission = document.getElementById('admission');
+    var religion = document.getElementById('religion');
+    var category = document.getElementById('category');
+    var email = document.getElementById('email');
+    var grade = document.getElementById('grade');
+    var section = document.getElementById('section');
+    var house = document.getElementById('house');
+    var admissionId = document.getElementById('admissionId');
+    var shortBio = document.getElementById('shortBio');
+    var studentPhoto = document.getElementById('studentPhoto');
 
 
     // Save the person in the database
-    database.addPerson(firstname.value, lastname.value, gender.value, dateOfBirth.value);
+    database.addPerson(firstname.value,
+      lastname.value,
+      gender.value,
+      dateOfBirth.value,
+      dateOfAdmission.value,
+      rollNumber.value,
+      contactNumber.value,
+      alternateNumber.value,
+      studentAadharNumber.value,
+      fatherAadharNumber.value,
+      fatherName.value,
+      motherAadharNumber.value,
+      motherName.value,
+      bloodGroup.value,
+      admission.value,
+      religion.value,
+      category.value,
+      email.value,
+      grade.value,
+      section.value,
+      house.value,
+      admissionId.value,
+      shortBio.value,
+      studentPhoto.value
+    );
 
     // Reset the input fields
     firstname.value = '';
     lastname.value = '';
     gender.value = '';
     dateOfBirth.value = '';
+    dateOfAdmission.value = '';
+    rollNumber.value = '';
+    contactNumber.value = '';
+    alternateNumber.value = '';
+    studentAadharNumber.value = '';
+    fatherAadharNumber.value = '';
+    fatherName.value = '';
+    motherAadharNumber.value = '';
+    motherName.value = '';
+    bloodGroup.value = '';
+    admission.value = '';
+    religion.value = '';
+    category.value = '';
+    email.value = '';
+    grade.value = '';
+    section.value = '';
+    house.value = '';
+    admissionId.value = '';
+    shortBio.value = '';
+    studentPhoto.value = '';
     // Repopulate the table
     populateTable();
   });
@@ -32,7 +96,7 @@ window.onload = function() {
 function populateTable() {
 
   // Retrieve the persons
-  database.getPersons(function(persons) {
+  database.getPersons(function (persons) {
 
     // Generate the table body
     var tableBody = '';
@@ -40,6 +104,29 @@ function populateTable() {
       tableBody += '<tr>';
       tableBody += '  <td>' + persons[i].firstname + '</td>';
       tableBody += '  <td>' + persons[i].lastname + '</td>';
+      tableBody += '  <td>' + persons[i].gender + '</td>';
+      tableBody += '  <td>' + persons[i].dateOfBirth + '</td>';
+      tableBody += '  <td>' + persons[i].dateOfAdmission + '</td>';
+      tableBody += '  <td>' + persons[i].rollNumber + '</td>';
+      tableBody += '  <td>' + persons[i].contactNumber + '</td>';
+      tableBody += '  <td>' + persons[i].alternateNumber + '</td>';
+      tableBody += '  <td>' + persons[i].studentAadharNumber + '</td>';
+      tableBody += '  <td>' + persons[i].fatherAadharNumber + '</td>';
+      tableBody += '  <td>' + persons[i].fatherName + '</td>';
+      tableBody += '  <td>' + persons[i].motherAadharNumber + '</td>';
+      tableBody += '  <td>' + persons[i].motherName + '</td>';
+      tableBody += '  <td>' + persons[i].bloodGroup + '</td>';
+      tableBody += '  <td>' + persons[i].admission + '</td>';
+      tableBody += '  <td>' + persons[i].religion + '</td>';
+      tableBody += '  <td>' + persons[i].category + '</td>';
+      tableBody += '  <td>' + persons[i].email + '</td>';
+      tableBody += '  <td>' + persons[i].grade + '</td>';
+      tableBody += '  <td>' + persons[i].section + '</td>';
+      tableBody += '  <td>' + persons[i].house + '</td>';
+      tableBody += '  <td>' + persons[i].admissionId + '</td>';
+      tableBody += '  <td>' + persons[i].shortBio + '</td>';
+      tableBody += '  <td>' + persons[i].studentPhoto + '</td>';
+
       tableBody += '  <td><input type="button" value="Delete" onclick="deletePerson(\'' + persons[i]._id + '\')"></td>'
       tableBody += '</tr>';
     }
